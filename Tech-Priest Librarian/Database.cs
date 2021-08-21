1892a7ebfd65800d730c029506afab2fc2a7fb44
Aleks <13088365+Bomjara-L@users.noreply.github.com>
2021-08-21 16:58:25 +0300

    Initial commit

---
+++ Tech-Priest Librarian/Database.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+    public enum Genre {};
+   public class ServerData
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public Genre Genre { get; set; }
+        public string DateAdded { get; set; }
+        public string WhoAdded { get; set; }
+    }
+   public static class Database
+    {
+        public static List<ServerData> servers = new List<ServerData>();
+        public static void Save()
+        {
+            foreach (var server in servers)
+            {
+                File.WriteAllText($"Servers/{server.Name}", JsonConvert.SerializeObject(server));
+            }
+        }
+        public static void Load()
+        {
+            foreach (var server in Directory.GetFiles("Servers"))
+            {
+                  servers.Add( JsonConvert.DeserializeObject<ServerData>(File.ReadAllText($"Servers/{server}")));      
+            }
+        }
+        public static string AddNewServer()
+        {
+            return "error";
+        }
+        public static string UpdateServers()
+        {
+            Load();
+            return "Сервера обновлены";
+        }
+        public static  bool RemoveServer()
+        {
+            return false;
+        }
+       
+
+}
---
+++ Tech-Priest Librarian/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Tech_Priest_Librarian
+{
+    class Config
+    {
+        public string Token { get; set; }
+    }
+    class Program
+    {
+        DiscordSocketClient client;
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            client.MessageReceived += CommandHandler; 
+            client.Log += Log;
+            Config config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(Directory.GetCurrentDirectory().ToString() + "/cfg.json")) ;
+            await client.LoginAsync(TokenType.Bot, config.Token);
+            await client.StartAsync();
+            Console.ReadLine();
+        }
+        //public string DataFromPath(string path)
+        //{
+        //    try
+        //    {
+        //        string rawJson;
+        //        using (var reader = new StreamReader(path))
+        //        {
+        //            rawJson = reader.ReadToEnd();
+        //        }
+
+        //        return JsonConvert.DeserializeObject<string>(rawJson);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.ToString());
+        //        return default(string);
+        //    }
+        //}
+
+        private Task CommandHandler(SocketMessage message)
+        {
+            if (!message.Author.IsBot && !message.Author.IsWebhook)
+            {
+                switch (message.Content)
+                {
+                    case "/Update servers":
+                        message.Channel.SendMessageAsync(Database.UpdateServers());
+                            break;
+                    case "/Add server":
+                        if (message.Author.PublicFlags.ToString().Contains("Librarian")) 
+                        {
+                            var serv = new ServerData();
+                            message.Channel.SendMessageAsync("Введите ссылку на сервер");
+                        }
+                        
+                        break;
+                }
+            }
+            
+            return Task.CompletedTask;
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
